a9e443eaba5a899f7977a08a7e6efc12702909bc
Eugene Klishevich <eklishevich@gmail.com>
2017-03-06 10:51:51 +0300

    Switch off logging in release configuration

--- MeasureTemprature/Prefix/Prefix_Defines.h
+++ MeasureTemprature/Prefix/Prefix_Defines.h
@@ -1,7 +1,7 @@
 #ifndef Prefix_Defines_h
 #define Prefix_Defines_h
 
-#if DEBUG || 1
+#if DEBUG
 #   define DebugLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
 #else
 #   define DebugLog(...)
